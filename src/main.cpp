3f419a13595368031e05e44e4b32280f67cc0e4f
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-03-28 20:26:11 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
 // main.cpp
 
-#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
-using namespace std;
+using namespace cv;
 
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!" << endl;
+    // Create a gui window
+    namedWindow("Output", 1);
+    
+    // Initialize a 120x350 matrix of black pixels
+    Mat output = Mat::zeros(120, 350, CV_8UC3);
+    
+    // Write text on the matrix
+    putText(output,
+            "Hello world :)",
+            cvPoint(15,70),
+            FONT_HERSHEY_PLAIN,
+            3,
+            cvScalar(0,255,0),
+            4);
+    
+    // Display the image
+    imshow("Output", output);
+    
+    // Wait for the user to press any key
+    waitKey(0);
 
     return 0;
 }
